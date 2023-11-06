85506db6dcfd0f35818a8a359c15ebd8d4731b08
Andrea Franchini <hello@andreafranchini.com>
2023-11-06 18:53:03 +0100

    add whitelist/blacklist for schema generator

--- tfmc/__main__.py
+++ tfmc/__main__.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from pprint import pprint
 from tfmc.cache import cache
@@ -5,37 +6,49 @@ from tfmc.class_transformer import transform
 from tfmc.loader import load_tf_model
 from tfmc.schema_gen import generate_schema
 
+argparser = argparse.ArgumentParser("TFMC")
+argparser.add_argument(
+    "-nc",
+    "--nocache",
+    help="Skips cached files.",
+    action=argparse.BooleanOptionalAction,
+)
+
+args = argparser.parse_args()
+
 # Enable colored text in output
-os.system('color')
+os.system("color")
 
 # Load the metamodel
-schema = cache('aws-schema-mm', lambda: generate_schema())
+if args.nocache:
+    schema = generate_schema()
+else:
+    schema = cache("aws-schema-mm", lambda: generate_schema())
 
 # Load a file by folder name
 # Path is /assets/examples/<name>
-model = load_tf_model('aws_ec2_ebs_docker_host')
+model = load_tf_model("aws_ec2_ebs_docker_host")
 
-resources = model['resources']
-data = model['data']
-variables = model['variable'] # do we need this?
-locals = model['locals'] # do we need this?
-output = model['output'] # do we need this?
+resources = model["resources"]
+data = model["data"]
+variables = model["variable"]  # do we need this?
+locals = model["locals"]  # do we need this?
+output = model["output"]  # do we need this?
 
 im_resources = []
 
 for category_and_id, props in resources.items():
-
-    [category, id] = category_and_id.split('.')
+    [category, id] = category_and_id.split(".")
 
     # just a double check to not get weird parsed stuff
-    assert category == props.get('__tfmeta').get('label')
+    assert category == props.get("__tfmeta").get("label")
 
     # check if metamodel specify this category
     if category in schema:
         schema_data = schema[category]
         im_resources.append(transform(category, id, props, model, schema_data))
     else:
-        print(f'Category \'{category}\' is unsupported by the TF Schema.')
+        print(f"Category '{category}' is unsupported by the TF Schema.")
 
 for res in im_resources:
     print(res)
--- tfmc/schema_gen.py
+++ tfmc/schema_gen.py
@@ -4,6 +4,8 @@ from importlib.resources import files
 import json
 import re
 import assets.schemas as schemas
+import yaml
+from yaml import Loader as YamlLoader
 
 """
 This module should take an AWS schema JSON file
@@ -12,60 +14,87 @@ generated with the terraform CLI:
 `terraform providers schema -json > aws-schema.json`
 """
 
+
 @dataclass
 class AWS_schemas:
     resources: dict[str, dict]
     data_sources: dict[str, dict]
 
-def _load_json_schema(data):
+
+def load_json_schema(data):
     schema = json.loads(data)
-    aws = schema['provider_schemas']['registry.terraform.io/hashicorp/aws']
-    aws_resource_schemas: dict[str, dict] = aws['resource_schemas']
-    aws_data_source_schemas: dict[str, dict] = aws['data_source_schemas']
+    aws = schema["provider_schemas"]["registry.terraform.io/hashicorp/aws"]
+    aws_resource_schemas: dict[str, dict] = aws["resource_schemas"]
+    aws_data_source_schemas: dict[str, dict] = aws["data_source_schemas"]
 
-    return AWS_schemas(aws_resource_schemas, 
-               aws_data_source_schemas)
+    return AWS_schemas(aws_resource_schemas, aws_data_source_schemas)
 
-def _get_possible_attrs_keys(attrs: dict, res_k: str):
+
+def get_possible_attrs_keys(attrs: dict, res_k: str, blacklist: list[str]):
     """
     Iterate attributes of a resource to find possible references to other resources.
     Their type should be 'string'.
     """
-    return [k for k, v in attrs.items() 
-                      if 'string' in v['type'] 
-                      and k != res_k
-                      and not k.endswith('_state')
-                      and not k.endswith('_type') ]
 
-def _add_mm_type_to_attributes(attrs, fk_attrs, res_k):
+    return [
+        k
+        for k, v in attrs.items()
+        if "string" in v["type"]
+        and k != res_k
+        and not k.endswith("_state")
+        and not k.endswith("_type")
+        and k not in blacklist
+    ]
+
+
+def add_mm_type_to_attributes(attrs, fk_attrs, res_k, custom_fks):
     """
     Add an attribute 'mm_type' to attributes that refer to another resource,
     with the infered type of that resource.
     """
-    attrs_keys = _get_possible_attrs_keys(attrs, res_k)
+
+    whitelist = custom_fks.get("whitelist") or {}
+    blacklist = custom_fks.get("blacklist") or {}
+
+    attrs_keys = get_possible_attrs_keys(attrs, res_k, blacklist)
     for k in attrs_keys:
         if fk_attrs_for_k := get_close_matches(k, [fk for fk in fk_attrs if fk in k]):
             # assign the best match as 'mm_type'
-            attrs[k]['mm_type'] = f'aws_{fk_attrs_for_k[0]}'
+            attrs[k]["mm_type"] = f"aws_{fk_attrs_for_k[0]}"
 
-def decorate_resource_schema_with_typed_associations(resources):
+    if res_k in whitelist:
+        for k, v in whitelist[res_k].items():
+            if k in attrs:
+                attrs[k]["mm_type"] = v
+
+
+def add_typed_associations_to_attrs(resources, custom_fks=None):
     """Returns an annotated schema with Terraform resource types"""
+    # Get foreign keys stripped of the 'aws_' prefix
+    fk_attrs = [re.sub(r"^aws_", "", fk) for fk in resources.keys()]
+
+    # For each resource, get its attributes and nested resources
     for res_k, res_v in resources.items():
-        # Get foreign keys stripped of the 'aws_' prefix
-        fk_attrs = fk_attrs = [re.sub(r'^aws_', '', fk) for fk in resources.keys()]
+        res_attrs = res_v["block"].get("attributes", {})
 
-        res_attrs = res_v['block'].get('attributes', {})
+        add_mm_type_to_attributes(res_attrs, fk_attrs, res_k, custom_fks)
 
-        _add_mm_type_to_attributes(res_attrs, fk_attrs, res_k)
-        
-        res_blocktypes = res_v['block'].get('block_types', {})
+        res_blocktypes = res_v["block"].get("block_types", {})
         for _, bt_v in res_blocktypes.items():
-            if bt_attrs := bt_v['block'].get('attributes', {}):
-                _add_mm_type_to_attributes(bt_attrs, fk_attrs, res_k)
-    
+            if bt_attrs := bt_v["block"].get("attributes", {}):
+                add_mm_type_to_attributes(bt_attrs, fk_attrs, res_k, custom_fks)
+
     return resources
 
+
 def generate_schema():
-    schema = files(schemas).joinpath('aws-schema.json').read_text()
-    aws_schemas = _load_json_schema(schema)
-    return decorate_resource_schema_with_typed_associations(aws_schemas.resources)
+    """Generates an annotated schema starting from the `aws-schema.json` input file
+    in the `assets` resource folder."""
+    schema = files(schemas).joinpath("aws-schema.json").read_text()
+    aws_schemas = load_json_schema(schema)
+
+    custom_fks = yaml.load(
+        files(schemas).joinpath("custom.yaml").read_text(), Loader=YamlLoader
+    )
+
+    return add_typed_associations_to_attrs(aws_schemas.resources, custom_fks)
