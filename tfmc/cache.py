39a02db78180c3b054a838c38089af69b64ec347
Andrea Franchini <hello@andreafranchini.com>
2024-01-02 16:07:49 +0100

    fix cache bug

--- tfmc/cache.py
+++ tfmc/cache.py
@@ -11,7 +11,7 @@ if not os.path.exists(cache_dir):
     os.mkdir(cache_dir)
 
 
-def cache(filename: str, encode_callback=lambda x: x, decode_callback=lambda x: x):
+def cache(filename: str, encode_callback, decode_callback):
     """Caches to disk an expensive function result like a JSON file.
     - `filename` the key to access the cached data
     - `encode_callback` a function that wraps the expensive function and encodes it
@@ -23,11 +23,11 @@ def cache(filename: str, encode_callback=lambda x: x, decode_callback=lambda x: 
         with open(file, "w") as fp:
             data = encode_callback()
             fp.write(data)
-            return data
+            return decode_callback(data)
     else:
         print(f"Cache for '{filename} found. Loading cache...'")
         with open(file) as fp:
-            data = json.loads(fp.read())
+            data = fp.read()
             return decode_callback(data)
 
 
